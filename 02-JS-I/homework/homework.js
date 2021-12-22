80754ff8ef33a7900885c8beae8e8d08248e4635
cinthia24027@gmail.com <cinthia24027@gmail.com>
2021-12-21 23:07:15 -0500

    PrimeraParte

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,19 +4,19 @@
 const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 25;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
@@ -26,12 +26,15 @@ const nuevoModulo = 21 % 5 === null;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
+  return str;
   // "Return" la string provista: str
   // Tu código:
   
 }
 
 function suma(x, y) {
+  var sum = x + y;
+  return sum;
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
@@ -39,18 +42,24 @@ function suma(x, y) {
 }
 
 function resta(x, y) {
+  var rest = x - y;
+  return rest;
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
   
 }
 
 function multiplica(x, y) {
+  var multip = x * y;
+  return multip;
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
   
 }
 
 function divide(x, y) {
+  var divis = x / y;
+  return divis;
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
   
